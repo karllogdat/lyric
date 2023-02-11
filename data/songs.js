b080a9bdb16938b1387f06f51fff7ecafe788d5e
Karl Logdat <logdatk@gmail.com>
2023-02-11 15:11:25 +0800

    Improved home

--- data/songs.js
+++ data/songs.js
@@ -1,23 +1,33 @@
 const songs = [
     {
         "title": "Non-Breath Oblige",
-        "artist": "PinocchioP"
+        "artist": "PinocchioP",
+        "link": "https://www.youtube.com/watch?v=lw7pcm1W5tw"
     }, 
     {
         "title": "Flyer!",
-        "artist": "Chinozo"
+        "artist": "Chinozo",
+        "link": "https://www.youtube.com/watch?v=nfpYhYtHuKo"
     },
     {
         "title": "Phony",
-        "artist": "Tsumiki"
+        "artist": "Tsumiki",
+        "link": "https://www.youtube.com/watch?v=9QLT1Aw_45s"
     },
     {
         "title": "Shoujorei",
-        "artist": "MikitoP"
+        "artist": "MikitoP",
+        "link": "https://www.youtube.com/watch?v=JW3N-HvU0MA"
     },
     {
         "title": "Brand New Day",
-        "artist": "iruace"
+        "artist": "iruace",
+        "link": "https://www.youtube.com/watch?v=2KrCmAWrrKI"
+    },
+    {
+        "title": "Pretender",
+        "artist": "Official Hige Dandism",
+        "link": "https://www.youtube.com/watch?v=TQ8WlA2GXbk"
     }
 ];
 
--- scripts/songlist.js
+++ scripts/songlist.js
@@ -8,10 +8,50 @@ const songListContainer = document.querySelector('#songlist');
 songList.sort((a, b) => (a.title > b.title) ? 1 : -1);
 
 for(const i in songList) {
+    // Creates the main container
     const newContainer = document.createElement("div");
     newContainer.className = "song-container";
-    
-    newContainer.innerHTML = songList[i].title;
 
+    // Creates the info container
+    const newInfo = document.createElement("div");
+    newInfo.className = "info";
+
+    // Creates link container 
+    const newLinkCon = document.createElement("div");
+    newLinkCon.className = "link-container";
+
+    // Creates the title div
+    const newTitle = document.createElement("div");
+    newTitle.className = "song-title";
+    newTitle.innerHTML = songList[i].title;
+
+    // Creates the artist div
+    const newArtist = document.createElement("div");
+    newArtist.className = "song-artist";
+    newArtist.innerHTML = songList[i].artist;
+
+    // Creates the icon
+    const newIcon = document.createElement("i");
+    newIcon.className = "fa-solid fa-headphones";
+
+    const newLink = document.createElement("a");
+    newLink.className = "song-link";
+    newLink.innerHTML = "Listen";
+    newLink.href = songList[i].link;
+    newLink.target = "_blank"
+
+    // Appends title and artist to info container
+    // Must be done 1st
+    newInfo.appendChild(newTitle);
+    newInfo.appendChild(newArtist);
+
+    newLinkCon.appendChild(newIcon);
+    newLinkCon.appendChild(newLink);
+
+    // Appends info container and icon to main container
+    newContainer.appendChild(newInfo);
+    newContainer.appendChild(newLinkCon);
+
+    // Appends finished container to songlist
     songListContainer.appendChild(newContainer);
 }
